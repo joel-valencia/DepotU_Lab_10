a9cb5b8147e9a42800e03e2086e170d5ce84d311
Joel Valencia <trekkerj1@gmail.com>
2016-03-09 13:47:54 -0600

    disable buttons and give wait message while waiting

--- scripts.js
+++ scripts.js
@@ -1,42 +1,56 @@
 var num1 = null;
 var num2 = null;
+var wait = "Please wait...";
+var numOfButtons = null;
+window.onload = function() {
+    numOfButtons = document.getElementsByTagName('button').length;
+}
 
-function getNums() {
+
+function prepare() {
     num1 = document.getElementById('num1').value;
     num2 = document.getElementById('num2').value;
+    document.getElementById('messages').innerHTML = wait;
+    for (var i = 0; i < numOfButtons; i++) {
+        document.getElementsByTagName('button')[i].disabled = true;
+    }
 }
 
 function callback(err, result) {
-        if (err) {
-            alert("Error!");
-        } else {
-            alert("Result: " + result);
-        }
+    document.getElementById('messages').innerHTML = "";
+    for (var i = 0; i < numOfButtons; i++) {
+        document.getElementsByTagName('button')[i].disabled = false;
+    }
+    if (err) {
+        alert(err);
+    } else {
+        alert("Result: " + result);
+    }
 }
 
 function add() {
-    getNums();
+    prepare();
     davidsLib.add(num1, num2, function(err, result) {
         callback(err, result);
     });
 }
 
 function subtract() {
-    getNums();
+    prepare();
     davidsLib.subtract(num1, num2, function(err, result) {
         callback(err, result);
     });
 }
 
 function multiply() {
-    getNums();
+    prepare();
     davidsLib.multiply(num1, num2, function(err, result) {
         callback(err, result);
     });
 }
 
 function divide() {
-    getNums();
+    prepare();
     davidsLib.divide(num1, num2, function(err, result) {
         callback(err, result);
     });
